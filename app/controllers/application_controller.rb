344f6068127178e5ee7fe4171ed213e7723d47a2
Awtanabe <panser098@gmail.com>
2019-10-27 20:12:57 +0900

    ユーザー一覧画面

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,7 @@ class ApplicationController < ActionController::Base
     # ここはaccount_update　編集のときらしい　上はsign_upだからcrudの操作それぞれでパラメーターを設定できるのか
     devise_parameter_sanitizer.permit(:account_update, keys: %i(name self_introduction sex img_name))
   end
+  def after_sign_in_path_for(resource)
+    users_path
+  end
 end
